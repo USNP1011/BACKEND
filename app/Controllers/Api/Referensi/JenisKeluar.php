648a2ac45145687a48609f4d8d3221b37e799ff4
ocph23 <ocph23@gmail.com>
2024-06-18 11:58:25 +0900

    apalah

--- app/Controllers/Api/Referensi/JenisKeluar.php
+++ app/Controllers/Api/Referensi/JenisKeluar.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers\Api\Referensi;
 
+use App\Models\JenisKeluarModel;
 use App\Models\JenisPendaftaranModel;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\RESTful\ResourceController;
@@ -10,7 +11,7 @@ class JenisPendaftaran extends ResourceController
 {
     public function store()
     {
-        $object = new JenisPendaftaranModel();
+        $object = new JenisKeluarModel();
         return $this->respond([
             'status' => true,
             'data' => $object->findAll()
