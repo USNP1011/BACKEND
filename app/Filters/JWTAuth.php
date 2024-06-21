006b51d80ea72d8906cbe5b7952a77e9ee78f0a0
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2024-06-21 11:23:48 +0900

    sd

--- app/Filters/JWTAuth.php
+++ app/Filters/JWTAuth.php
@@ -32,6 +32,9 @@ class JWTAuth implements FilterInterface
      */
     public function before(RequestInterface $request, $arguments = null)
     {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         $authenticationHeader = $request->getServer('HTTP_AUTHORIZATION');
         try {
             $encodedToken = getJWTFromRequest($authenticationHeader);
