4a067294cf400a6d9bef9125b2bddb1fc97305ed
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2024-06-21 13:01:07 +0900

    sd

--- app/Controllers/Api/Referensi/Agama.php
+++ app/Controllers/Api/Referensi/Agama.php
@@ -3,11 +3,12 @@
 namespace App\Controllers\Api\Referensi;
 
 use App\Models\AgamaModel;
+use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\RESTful\ResourceController;
 
 class Agama extends ResourceController
 {
-    public function store()
+    public function store():ResponseInterface
     {
         $object = new AgamaModel();
         return $this->respond([
