0c0255c3f0c7f8d2890afb2f0b0448ce19f655bd
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2024-08-06 18:29:57 +0900

    sds

--- app/Entities/KelasKuliahEntity.php
+++ app/Entities/KelasKuliahEntity.php
@@ -10,4 +10,9 @@ class KelasKuliahEntity extends Entity
     protected $datamap = [];
     protected $dates   = ['tanggal_mulai_efektif', 'tanggal_akhir_efektif', 'sync_at', 'created_at', 'updated_at', 'deleted_at'];
     protected $casts   = [];
+
+    public function getNamaMataKuliah() {
+        $this->attributes['nama_mata_kuliah'] = strtoupper($this->original['nama_mata_kuliah']);
+        return $this->attributes['nama_mata_kuliah'];
+    }
 }
