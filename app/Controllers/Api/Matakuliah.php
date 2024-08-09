b5bf9949aa94937de88b90fc307a3050c0796ba8
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2024-08-09 15:39:49 +0900

    matakuliah

--- app/Controllers/Api/Matakuliah.php
+++ app/Controllers/Api/Matakuliah.php
@@ -148,6 +148,8 @@ class Matakuliah extends ResourceController
             'data' => $object->select('matakuliah.kode_mata_kuliah, matakuliah.nama_mata_kuliah, matakuliah.sks_mata_kuliah, matakuliah.id_jenis_mata_kuliah, prodi.nama_program_studi, matakuliah.status_sync, matakuliah.sync_at')
             ->join('prodi', 'prodi.id_prodi=matakuliah.id_prodi')
             ->like('nama_mata_kuliah', $param->cari)
+            ->orLike('kode_mata_kuliah', $param->cari)
+            ->orLike('prodi.nama_program_studi', $param->cari)
             ->orderBy(isset($param->order) && $param->order->field != "" ? $param->order->field : 'prodi.nama_program_studi', isset($param->order) && $param->order->field != "" ? $param->order->direction : 'desc')
             ->paginate($param->count, 'default', $param->page),
             'pager' => $object->pager->getDetails()
