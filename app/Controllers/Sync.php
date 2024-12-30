496ac2ebad9f211f8a55faa567fb0dd5d9f09f01
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2024-12-30 18:35:06 +0900

    nilai

--- app/Controllers/Sync.php
+++ app/Controllers/Sync.php
@@ -624,4 +624,44 @@ class Sync extends BaseController
             return $this->fail($record);
         }
     }
+
+    function syncNilaiPesertaKelas()
+    {
+        $object = \Config\Database::connect();
+        $record = ['berhasil' => [], 'gagal' => []];
+        try {
+            $data = $object->query("SELECT nilai_kelas.*, kelas_kuliah.id_kelas_kuliah, riwayat_pendidikan_mahasiswa.id_registrasi_mahasiswa, mahasiswa.nama_mahasiswa, (if(nilai_kelas.status_sync is null AND nilai_kelas.deleted_at is null, 'insert', if(nilai_kelas.status_sync is not null AND nilai_kelas.deleted_at is null and nilai_kelas.sync_at<nilai_kelas.updated_at, 'update', if(nilai_kelas.status_sync is not null and nilai_kelas.deleted_at is not null and nilai_kelas.sync_at<nilai_kelas.deleted_at,'delete', null)))) as set_sync 
+            FROM nilai_kelas 
+            LEFT JOIN peserta_kelas on peserta_kelas.id=nilai_kelas.id_nilai_kelas
+            LEFT JOIN riwayat_pendidikan_mahasiswa on riwayat_pendidikan_mahasiswa.id=peserta_kelas.id_riwayat_pendidikan
+            LEFT JOIN mahasiswa on mahasiswa.id=riwayat_pendidikan_mahasiswa.id_mahasiswa
+            LEFT JOIN kelas_kuliah on kelas_kuliah.id=peserta_kelas.kelas_kuliah_id 
+            WHERE if(nilai_kelas.status_sync is null AND nilai_kelas.deleted_at is null, 'insert', if(nilai_kelas.status_sync is not null AND nilai_kelas.deleted_at is null and nilai_kelas.sync_at<nilai_kelas.updated_at, 'update', if(nilai_kelas.status_sync is not null and nilai_kelas.deleted_at is not null and nilai_kelas.sync_at<nilai_kelas.deleted_at,'delete', null))) IS NOT NULL")->getResult();
+            $batchSize = 100; // Ukuran batch yang lebih kecil
+            foreach (array_chunk($data, $batchSize) as $batch) {
+                foreach ($batch as $key => $value) {
+                    $key = (object) [
+                        'id_registrasi_mahasiswa' => $value->id_registrasi_mahasiswa,
+                        'id_kelas_kuliah' => $value->id_kelas_kuliah
+                    ];
+                    $setData = (object) [
+                        'nilai_angka'=>$value->nilai_angka,
+                        'nilai_huruf'=>$value->nilai_huruf,
+                        'nilai_indeks'=>$value->nilai_indeks
+                    ];
+                    $result = $this->api->updateData('UpdateNilaiPerkuliahanKelas', $this->token, $setData, $key);
+                    if ($result->error_code == "0") {
+                        $query = "UPDATE nilai_kelas SET sync_at = '" . date('Y-m-d H:i:s') . "', status_sync='sudah sync' WHERE id = '" . $value->id_nilai_kelas . "'";
+                        $object->query($query);
+                        $record['berhasil'][] = $key;
+                    } else {
+                        $record['gagal'][] = $key;
+                    }
+                }
+            }
+            return $this->respond($record);
+        } catch (\Throwable $th) {
+            return $this->fail($record);
+        }
+    }
 }
