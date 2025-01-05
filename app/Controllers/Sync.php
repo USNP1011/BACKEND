fc849b112e5271c509d1c96492ada9b529be8aa0
ocph23 <ocph23@gmail.com>
2025-01-05 17:40:27 +0900

    sync

--- app/Controllers/Sync.php
+++ app/Controllers/Sync.php
@@ -24,7 +24,7 @@ class Sync extends BaseController
     }
     public function index()
     {
-        // return view('syn');
+        return view('sync');
     }
 
     public function getSync(): object
