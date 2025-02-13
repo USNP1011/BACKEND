5418e112690a4bc884309290333259292d686741
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2025-02-13 17:04:43 +0900

    kurikulum

--- app/Controllers/Api/V2/Kurikulum.php
+++ app/Controllers/Api/V2/Kurikulum.php
@@ -12,7 +12,7 @@ class Kurikulum extends BaseController
         $object = new MatakuliahKurikulumModel();
         return $this->respond([
             'status' => true,
-            'data' => $object->select("matakuliah.kode_mata_kuliah, matakuliah.nama_mata_kuliah, matakuliah_kurikulum.sks_mata_kuliah, matakuliah_kurikulum.sks_praktek, matakuliah_kurikulum.sks_praktek_lapangan, matakuliah_kurikulum.sks_simulasi, matakuliah_kurikulum.semester, matakuliah_kurikulum.apakah_wajib")
+            'data' => $object->select("matakuliah.kode_mata_kuliah, matakuliah.nama_mata_kuliah, matakuliah_kurikulum.sks_mata_kuliah, matakuliah_kurikulum.sks_tatap_muka, matakuliah_kurikulum.sks_praktek, matakuliah_kurikulum.sks_praktek_lapangan, matakuliah_kurikulum.sks_simulasi, matakuliah_kurikulum.semester, matakuliah_kurikulum.apakah_wajib")
             ->join('matakuliah', 'matakuliah.id=matakuliah_kurikulum.matakuliah_id', 'left')
             ->where('kurikulum_id', $id)
             ->orderBy('semester')
